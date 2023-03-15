b7556ee721ba11b9b75e698b2f718f2fb7fdba03
Dimdim28 <wwwdimanes1@gmail.com>
2023-03-15 20:07:26 +0200

    feature: added courses-page component

---
+++ src/components/pages/courses-page/courses-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './courses-page.module.scss';
+
+const CoursesPage = () => {
+  return <div className={styles.wrapper}>Courses page</div>;
+};
+
+export default CoursesPage;
