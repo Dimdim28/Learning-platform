6bd864f812d8c1a8497bf6dd380995980689edbf
Dimdim28 <wwwdimanes1@gmail.com>
2023-03-16 16:29:48 +0200

    feature: added lessons page

---
+++ src/components/pages/lessons-page/lessons-page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './lessons-page.module.scss';
+
+const LessonsPage = () => {
+  return <div className={styles.wrapper}>Lessons page</div>;
+};
+
+export default LessonsPage;
