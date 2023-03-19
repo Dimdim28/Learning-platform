68763ba7d62e6fdb1dde2ebb4748c2d5f4e7020f
Dimdim28 <wwwdimanes1@gmail.com>
2023-03-19 20:58:31 +0200

    fix: moved token to the .env file

--- src/services/instance.ts
+++ src/services/instance.ts
@@ -4,8 +4,7 @@ const axiosInstance = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_API_HOST}/${process.env.NEXT_PUBLIC_API_VERSION}`,
   headers: {
     'Content-Type': 'application/json',
-    Authorization:
-      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkYjFhODEwYy1iZGNjLTRiY2ItYTQyMC0xOTQ3MjRkZGMxNmMiLCJwbGF0Zm9ybSI6InN1YnNjcmlwdGlvbnMiLCJpYXQiOjE2Nzg5MDYxMDMsImV4cCI6MTY3OTgwNjEwM30.en1Z4lf-sUfsh3eW9rGE-cexL7-IBiI73TrlFJzJ09U',
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`,
   },
 });
 
