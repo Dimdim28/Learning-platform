2630d1f29c89ce6fc9282b9402216f2279815d3e
Dimdim28 <wwwdimanes1@gmail.com>
2023-03-19 13:31:49 +0200

    feature: added ability to change video playing speed with '0' and '9' buttons

--- src/components/pages/lessons-page/components/VideoCard/VideoCard.tsx
+++ src/components/pages/lessons-page/components/VideoCard/VideoCard.tsx
@@ -26,6 +26,29 @@ const VideoCard: React.FC<VideoCardProps> = ({ src, title, poster }) => {
     getCurrentTime();
   }, [videoRef, src]);
 
+  useEffect(() => {
+    const handleKeyDown = (event: {
+      ctrlKey: any;
+      key: string;
+      preventDefault: () => void;
+    }) => {
+      const current = videoRef.current;
+      if (event.key === '0' && current?.playbackRate) {
+        event.preventDefault();
+        if (current.playbackRate < 2) current.playbackRate += 0.1;
+      } else if (event.key === '9' && current?.playbackRate) {
+        event.preventDefault();
+        if (current.playbackRate > 0.5) current.playbackRate -= 0.1;
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       {src ? (
